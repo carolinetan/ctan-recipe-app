35c10afd6e4ed6854f89584e369eb738f8c7b273
Caroline Tan <emailctan@gmail.com>
2018-08-14 17:37:44 -0400

    initial implementation - adding one-to-one JPA implementation

---
+++ src/main/java/udemy/course/CtanRecipeAppApplication.java
@@ -0,0 +1,12 @@
+package udemy.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtanRecipeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtanRecipeAppApplication.class, args);
+    }
+}
---
+++ src/main/java/udemy/course/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package udemy.course.domain;
+
+/**
+ * Created by jt on 6/13/17.
+ */
+public enum Difficulty {
+
+    EASY, MODERATE, KIND_OF_HARD, HARD
+}
